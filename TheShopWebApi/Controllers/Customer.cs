f8859381e4560f0468d4a2f48c5ea0a89114c288
Mehran Ayub <mehran.ayub@stgsystems.com>
2024-01-10 18:27:36 +0500

    Added Configuration Files

--- TheShopWebApi/Controllers/Customer.cs
+++ TheShopWebApi/Controllers/Customer.cs
@@ -17,73 +17,26 @@ namespace TheShopWebApi.Controllers
     public class Customer : ControllerBase
     {
         private readonly IRepositoryWrapper _repository;
-        private readonly IDatabase _database;
-
-        // private readonly IDistributedCache _distributedCache;
-        //private string KeyName = "Master";
 
         public Customer(IRepositoryWrapper repositoryWrapper)
         {
             _repository = repositoryWrapper;
-            var connection = ConnectionMultiplexer.Connect("localhost:6379");
-            _database = connection.GetDatabase();
-
         }
-        //public Customer(IRepositoryWrapper repositoryWrapper, IDistributedCache distributedCache)
-        //{
-        //    _repository = repositoryWrapper;
-        //    _distributedCache = distributedCache;
-
-        //}
         [HttpGet]
         public async Task< IActionResult> GetAllCustomers()
         {
-            
             try
             {
-                string SerializeList = string.Empty;
-
-                string keyName = "Master";
-                string searchedValue = "FName";
-
-                // Define the Lua script
-                string luaScript = "return redis.call('GET', KEYS[1], ARGV[1])";
-
-                // Create the Lua script with the LuaScript class
-                LuaScript script = LuaScript.Prepare(luaScript);
-
-                // Execute the script and get the result
-                RedisValue result = (RedisValue)script.Evaluate(_database, new { keys = new RedisKey[] { keyName }, args = new RedisValue[] { searchedValue } });
-
-
-         
-
-                if (result.HasValue)
-                {
-                    return Ok(result.ToString());
-                }
-                else
-                {
                     var customers = _repository.Customer.GetAllCustomers();
                     if (customers != null)
                     {
-                        SerializeList = JsonConvert.SerializeObject(customers);
-                        //EncodedList = Encoding.UTF8.GetBytes(SerializeList);
-                        //var Option = new DistributedCacheEntryOptions()
-                        //    .SetSlidingExpiration(TimeSpan.FromMinutes(20)) // After 20 min Entry will be Inactive
-                        //    .SetAbsoluteExpiration(DateTime.Now.AddHours(6)); // Expired in 6 hour
-                         _database.StringSet(keyName, SerializeList);
                         return Ok(customers);
                     }
                     else
                     {
                         return Ok(null);
                     }
-
-               }
-                    
-                
-                
+ 
             }
             catch (Exception ex)
             {
